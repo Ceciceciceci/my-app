45b4d852e6a518bcd55f6d25acab8ad27f85934b
Ceci Tran <ceci@Cecis-MacBook-Pro.local>
2020-06-16 23:22:29 -0700

    Updates

---
+++ precache-manifest.d7deff9f99babf022355948487aa1041.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6573ecab7805f053596e06ce919cc11",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1eee5b38e59cda96290a",
+    "url": "/my-app/static/css/main.3cec226e.chunk.css"
+  },
+  {
+    "revision": "7d90b83b9af23bc25df8",
+    "url": "/my-app/static/js/2.2efce830.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.2efce830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eee5b38e59cda96290a",
+    "url": "/my-app/static/js/main.3ce5cc47.chunk.js"
+  },
+  {
+    "revision": "21df01211e74f30977a1",
+    "url": "/my-app/static/js/runtime-main.9a1f6c18.js"
+  },
+  {
+    "revision": "280b8866d596322c30abb127c3b67981",
+    "url": "/my-app/static/media/732d93c0-0db5-4976-bfb4-cd1ae5bc5899.280b8866.woff"
+  },
+  {
+    "revision": "457957fb7516317dfa7d3214d1711678",
+    "url": "/my-app/static/media/a16ba49e-2f15-4cfd-9668-c7768fa1059b.457957fb.woff2"
+  },
+  {
+    "revision": "4e9e54b64efb7c07a6abb081c1caf854",
+    "url": "/my-app/static/media/behance.4e9e54b6.svg"
+  },
+  {
+    "revision": "429913d641737773f99e3ae32261b9d5",
+    "url": "/my-app/static/media/dogbread2.429913d6.png"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/my-app/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/my-app/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/my-app/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "b0743d0c8567a71bdb71a97cdde52831",
+    "url": "/my-app/static/media/proj1-img.b0743d0c.jpg"
+  },
+  {
+    "revision": "280a5f3fca98e7c59740916daee0bec2",
+    "url": "/my-app/static/media/proj2-img.280a5f3f.jpg"
+  }
+]);
